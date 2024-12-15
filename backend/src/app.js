57c56d13d7ae93cbf55a2f90984394a2e6a7c4f1
VSikorski <sikorski.vitalik@gmail.com>
2024-12-15 22:22:45 +0200

    db and node app containerization

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,16 @@ const frontendPath = path.join(__dirname, '../../frontend');
 app.use(express.json());
 app.use(express.static(frontendPath));
 
+/* ---------------------------------- REST API ---------------------------------- */
+
+app.get('/api', (req, res) => {
+    res.send('Welcome to the HW api\n');
+})
+
+
+
+/* ------------------------------ DOCUMENT SERVING ------------------------------ */
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(frontendPath, 'index.html'));
 })
