986bb799125f17451f9fe4cfc8e42d18e217227f
VSikorski <sikorski.vitalik@gmail.com>
2025-01-03 22:40:37 +0200

    created endpoint for bulk series insertions and implemnted the functionality from the UI

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,12 @@ app.get('/api', (req, res) => {
 
 app.post('/api/post/series', async (req, res) => {
     const seriesData = req.body;
-    if (!seriesData.id) {
+
+    seriesData.id = parseInt(series.id, 10);
+    seriesData.year = parseInt(series.year, 10);
+    seriesData.setCount = parseInt(series.setCount, 10);
+
+    if (isNaN(series.id)) {
         return res.status(400).json({
             error: '`id` is a required field'
         });
@@ -40,6 +45,12 @@ app.post('/api/post/series', async (req, res) => {
             error: '`setCount` is a required field'
         });
     }
+
+    const dupSeries = await Series.findByPk(seriesData.id);
+    if (dupSeries) {
+        return res.status(400).json({ error: `Series record with specified id '${seriesData.id}' already exists` });
+    }
+
     try {
         const series = await Series.create(seriesData);
         return res.status(201).json({ message: 'Series record successfully inserted', data: series });
@@ -48,6 +59,73 @@ app.post('/api/post/series', async (req, res) => {
         return res.status(500).json({ error: 'Error occurred while inserting series record' });
     }
 })
+
+app.post('/api/post/series/bulk', async (req, res) => {
+    const seriesData = req.body;
+    let seriesArr = [];
+    let messageLog = '';
+    
+    if (!Array.isArray(seriesData)) {
+        return res.status(400).json({ error: 'Message body must be an array.' });
+    }
+    if (seriesData.length === 0) {
+        return res.status(400).json({ error: 'Array cannot be empty.' });
+    }
+
+    const operations = seriesData.map(async (series) => {
+        series.id = parseInt(series.id, 10);
+        series.year = parseInt(series.year, 10);
+        series.setCount = parseInt(series.setCount, 10);
+
+        if (isNaN(series.id)) {
+            messageLog += '- Series is missing a valid `id` and was skipped.\n';
+            return;
+        }
+        if (!series.type) {
+            messageLog += `- Series with id: ${series.id} is missing the required parameter \`type\` and was skipped.\n`;
+            return;
+        }
+        if (!series.name) {
+            messageLog += `- Series with id: ${series.id} is missing the required parameter \`name\` and was skipped.\n`;
+            return;
+        }
+        if (isNaN(series.year)) {
+            messageLog += `- Series with id: ${series.id} is missing a valid \`year\` and was skipped.\n`;
+            return;
+        }
+        if (isNaN(series.setCount)) {
+            messageLog += `- Series with id: ${series.id} is missing a valid \`setCount\` and was skipped.\n`;
+            return;
+        }
+
+        const dupSeries = await Series.findByPk(series.id);
+        if (dupSeries) {
+            messageLog += `- Series with id: ${series.id} contains a duplicate in the database and was skipped.\n`;
+            return;
+        }
+
+        try {
+            const newSeries = await Series.create(series);
+            seriesArr.push(newSeries);
+        } catch (error) {
+            console.log(error);
+            messageLog += `- Error creating a new series record with id: ${series.id}. Error: ${error.message}\n`;
+        }
+    });
+
+    await Promise.all(operations);
+
+    const responseMessage = seriesArr.length > 0 
+        ? 'Series were successfully inserted.'
+        : 'No records were inserted.';
+    
+    return res.status(seriesArr.length > 0 ? 201 : 400).json({ 
+        message: responseMessage, 
+        log: messageLog, 
+        insertedRecords: seriesArr 
+    });
+});
+
 
 app.get('/api/get/series/all', async (req,res) => {
     const {year} = req.query;
