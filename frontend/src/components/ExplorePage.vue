3ffe6428d8fe273a1d3baa424786df4f1c77e550
VSikorski <sikorski.vitalik@gmail.com>
2025-01-04 16:06:04 +0200

    Initial explore page pagination

--- frontend/src/components/ExplorePage.vue
+++ frontend/src/components/ExplorePage.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="explore-page container">
     <h6 class="explore-page-header">Available cars:</h6>
+   
     <ul class="explore-page-list">
-      <li v-for="item in items" :key="item.id" class="explore-page-item"><CarCard :car="item"/></li>
+      <li v-for="item in paginatedItems" :key="item.id" class="explore-page-item"><CarCard :car="item" /></li>
     </ul>
+
+
+    <!-- Pagination Controls -->
+    <div class="pagination" v-if="totalPages > 0">
+      <button @click="changePage(currentPage - 1)" :disabled="currentPage === 1">Previous</button>
+      
+      <button v-for="page in totalPages" :key="page" @click="changePage(page)" :class="{ active: currentPage === page }">{{ page }}</button>
+      
+      <button @click="changePage(currentPage + 1)" :disabled="currentPage === totalPages">Next</button>
+    </div>
+
   </div>
 </template>
 
@@ -20,13 +32,30 @@ export default {
   },
   data() {
     return {
-      items: []
+      items: [],
+      currentPage: 1,
+      itemsPerPage: 50,
     }
   },
   mounted() {
     this.fetchItems();
   },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.items.length / this.itemsPerPage);
+    },
+    paginatedItems() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      const end = start + this.itemsPerPage;
+      return this.items.slice(start, end);
+    },
+  },
   methods: {
+    changePage(page) {
+      if (page >= 1 && page <= this.totalPages) {
+        this.currentPage = page;
+      }
+    },
     async fetchItems() {
       try {
         const response = await axios.get('http://localhost:3000/api/get/cars');
